b797531d6192d67e4e600bbb3f90963a3717d3c8
Ketan-prajapati-1995 <ketanprajapati1789@gmail.com>
2024-06-05 13:06:44 +0530

    Initial commit

---
+++ app/(tabs)/Component_1/Register.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <View>
+      <Text style ={{backgroundColor:"blue",}}>My name is ketan prajapati</Text>
+    </View>
+  )
+}
+
+export default Register
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Register from './Component_1/Register'
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      <Register/>
+
+    </View>
+  )
+}
+
+export default index
